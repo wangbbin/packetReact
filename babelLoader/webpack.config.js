75c9a424a9444c43e196cd42cce0c1d7b95f7dbd
wangbbin <wbb499477096@qq.com>
2017-07-09 13:46:42 +0800

    babel-loader

---
+++ babelLoader/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders:[
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+    ]
+   /* loaders: [
+    {
+      test: /\.js[x]?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }
+  ]*/
+  }
+};
