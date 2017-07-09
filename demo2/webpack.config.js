e07700fce1778a54d95d604afab7c5f4caf4dc54
wangbbin <wbb499477096@qq.com>
2017-07-09 10:46:57 +0800

    multi entry

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,9 +1,27 @@
 module.exports = {
-  entry: {
+/*  entry: {
     bundle1: './main1.js',
     bundle2: './main2.js'
   },
   output: {
     filename: '[name].js'
-  }
+  }*/
+
+/*  entry: ['./main1', './main2'],
+  output: {
+    filename: 'bundle.js'
+  }*/
+
+  entry: {
+        page1: "./main2",
+        page2: ["./main1", "./main2"]
+    },
+    output: {
+        // Make sure to use [name] or [id] in output.filename
+        //  when using multiple entry points
+        filename: "[name].bundle.js",
+        chunkFilename: "[id].bundle.js",
+        path: __dirname+'/dist',
+        publicPath: '/static/'
+    }
 };
